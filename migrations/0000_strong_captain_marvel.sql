4dfb3e85fe28996644bac4525705c82ee1131114
Belkız Özbek <ceng.belkiz@gmail.com>
2025-06-23 01:51:36 +0300

    This change adds a new column named `pageCount` to the `books` table.
    The `pageCount` field is required to calculate the top readers of the month based on the total number of pages they’ve read. This data will be displayed on the statistics page.
    
    The migration was automatically generated using Drizzle Kit.

---
+++ migrations/0000_strong_captain_marvel.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "books" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"author" text NOT NULL,
+	"isbn" text NOT NULL,
+	"genre" text NOT NULL,
+	"publish_year" integer NOT NULL,
+	"shelf_number" text NOT NULL,
+	"available_copies" integer DEFAULT 1 NOT NULL,
+	"total_copies" integer DEFAULT 1 NOT NULL,
+	CONSTRAINT "books_isbn_unique" UNIQUE("isbn")
+);
+--> statement-breakpoint
+CREATE TABLE "borrowings" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"book_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
+	"borrow_date" date NOT NULL,
+	"due_date" date NOT NULL,
+	"return_date" date,
+	"status" text DEFAULT 'borrowed' NOT NULL,
+	"extension_requested" boolean DEFAULT false,
+	"notes" text
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"is_admin" boolean DEFAULT false NOT NULL,
+	"membership_date" date NOT NULL,
+	"admin_rating" integer DEFAULT 0,
+	"admin_notes" text,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "borrowings" ADD CONSTRAINT "borrowings_book_id_books_id_fk" FOREIGN KEY ("book_id") REFERENCES "public"."books"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "borrowings" ADD CONSTRAINT "borrowings_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
