a046148c62b28235f92c1e2e4141e34392658047
Belkız Özbek <ceng.belkiz@gmail.com>
2025-07-05 00:43:18 +0300

    gereksiz dosyalar silindi

--- migrations/0001_broad_dakota_north.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "books" ADD COLUMN "page_count" integer DEFAULT 0 NOT NULL;
--- migrations/0002_narrow_vindicator.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "books" ALTER COLUMN "shelf_number" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "books" ALTER COLUMN "page_count" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "books" ALTER COLUMN "page_count" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "books" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;
