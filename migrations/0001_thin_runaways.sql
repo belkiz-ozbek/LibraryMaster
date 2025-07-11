efb16e6c5d459818cefbc21c9a31bfd281376005
Belkız Özbek <ceng.belkiz@gmail.com>
2025-07-11 15:41:45 +0300

    chore: remove old migration files and reset migration history

--- migrations/0001_thin_runaways.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "username" text;--> statement-breakpoint
-UPDATE "users" SET "username" = 'user_' || id WHERE "username" IS NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "username" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_username_unique" UNIQUE("username");
